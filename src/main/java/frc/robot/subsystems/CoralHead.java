7a9ea84d9f5cdb76eda87ef263af100d74d36904
Ryan Blue <ryanzblue@gmail.com>
2025-02-25 09:15:53 -0500

    Add sensors to coral head

--- src/main/java/frc/robot/subsystems/CoralHead.java
+++ src/main/java/frc/robot/subsystems/CoralHead.java
@@ -1,17 +1,25 @@
 package frc.robot.subsystems;
 
+import java.util.function.DoubleSupplier;
+
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
+
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import java.util.function.DoubleSupplier;
 import monologue.Logged;
+import monologue.Annotations.Log;
 
 public class CoralHead extends SubsystemBase implements Logged {
   private TalonFX motor = new TalonFX(51);
   private DutyCycleOut dcRequest = new DutyCycleOut(0);
+
+  private DigitalInput rearSensor = new DigitalInput(10);
+  private DigitalInput midSensor = new DigitalInput(11);
+  private DigitalInput frontSensor = new DigitalInput(12);
 
   public CoralHead() {
     TalonFXConfiguration configuration = new TalonFXConfiguration();
@@ -36,6 +44,21 @@ public class CoralHead extends SubsystemBase implements Logged {
 
   public Command outputCommand(DoubleSupplier output) {
     return run(() -> setOutput(output.getAsDouble())).finallyDo(this::stop);
+  }
+
+  @Log(key = "rear sensor")
+  public boolean getRearSensor() {
+    return rearSensor.get();
+  }
+
+  @Log(key = "mid sensor")
+  public boolean getMidSensor() {
+    return midSensor.get();
+  }
+
+  @Log(key = "front sensor")
+  public boolean getFrontSensor() {
+    return frontSensor.get();
   }
 
   @Override
